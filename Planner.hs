f7cc50912b1e4e5fce5f74c555fc5668feae371c
paul-pothead <71666474+paul-pothead@users.noreply.github.com>
2020-11-20 20:27:29 +0500

    Fixed silly mistakes

--- Planner.hs
+++ Planner.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Planner where
 
 import Data.Bifunctor
@@ -5,41 +7,40 @@ import Data.Bifunctor
 import qualified Audio as A
 import qualified Data.List as L
 import qualified Data.Map as M
+import qualified Data.Sequence as Q
 
 import Array (aget, aset, Array)
 
 compareBy f a b = f a `compare` f b
 
-type Knobs = M.Map String Double
-
-
 data Tempo = Tempo
   {tempoStart :: Double,
    bpm        :: Double}
+
+defaultTempi = [Tempo 0 120] 
 
 deleteTempo :: Double -> [Tempo] -> [Tempo]
 deleteTempo slider = filter $ (slider /=) . tempoStart
 
 addTempo :: Double -> Double -> [Tempo] -> [Tempo]
-addTempo start bpm =
-  L.insertBy (compareBy tempoStart) (Tempo start bpm)
+addTempo slider bpm =
+  L.insertBy (compareBy tempoStart) (Tempo slider bpm)
+  . deleteTempo slider
 
 
 
-bufferBeats :: Int -> Double -> Double -> [Tempo] -> Double
-bufferBeats rate slider sampLeft (t1 : ts)
+tick :: Double -> Double -> Double -> [Tempo] -> Double
+tick rate slider buffSamp (t1 : ts)
 
-  | slider < tempoStart t1 = bufferBeats rate slider sampLeft ts
-
-  | null ts || sampTillNext >= sampLeft = sampLeft / rate' / 60 / bpm t1
+  | null ts                  = newSlider
+  | slider >= tempoStart t2  = tick rate slider buffSamp ts
+  | sampTillNext >= buffSamp = newSlider
   
-  | otherwise = tempoStart t2 - slider
-              + bufferBeats rate (tempoStart t2) (sampLeft - sampTillNext) ts
+  | otherwise = tick rate (tempoStart t2) (buffSamp - sampTillNext) ts
 
-  where rate' = fromIntegral rate
-        t2 = head ts
-        sampTillNext = 60 * rate' * (tempoStart t2 - slider) / bpm t1
-
+  where t2           = head ts
+        sampTillNext = 60 * rate * (tempoStart t2 - slider) / bpm t1
+        newSlider    = buffSamp / rate / 60 * bpm t1 + tempoStart t1
 
 
 softTempoChange :: Int -> Double -> [Tempo] -> [Tempo]
@@ -55,38 +56,41 @@ softTempoChange n slider (t1 : tail@(t2 : ts))
 
 
 
-windowSamples :: Int -> Double -> Double -> [Tempo] -> Int
-windowSamples rate start end (t1 : ts)
+window :: Double -> Double -> Double -> [Tempo] -> Double
+window rate start end (t1 : ts)
   
-  | start > end = negate (windowSamples rate end start ts)
+  | start > end = negate (window rate end start ts)
+  | null ts     = samples $ end - tempoStart t1
 
-  | start < tempoStart t1 = windowSamples rate start end ts
+  | start >= tempoStart t2 = window rate start end ts
+  | end <= tempoStart t2 = samples $ end - tempoStart t1
 
-  | null ts || end <= tempoStart t2 = sampTillNext
+  | otherwise = samples (tempoStart t2 - tempoStart t1)
+              + window rate (tempoStart t2) end ts
 
-  | otherwise = sampTillNext + windowSamples rate (tempoStart t2) end ts
-
-  where t2 = head ts
-        sampTillNext = round $ 60 * fromIntegral rate * (end - start) / bpm t1
-
+  where t2            = head ts
+        samples beats = 60 * rate * beats / bpm t1
 
 
-noteSamples :: Int -> Double -> Int -> [Tempo] -> Int
+
+noteSamples :: Double -> Double -> Double -> [Tempo] -> Double
 noteSamples rate start divisor tempi =
-  windowSamples rate start (4 / fromIntegral divisor + start) tempi
+  window rate start (4 / divisor + start) tempi
 
+data State = State
+  {knobs :: M.Map String Double,
+   accum :: Q.Seq A.Audio}
 
 
 data Effect = Effect
   {effectName :: String,
-   knobs      :: Knobs,
-   run        :: (Int -> A.Audio -> Knobs -> (A.Audio, Effect))}
+   state      :: State,
+   run        :: (Double -> A.Audio -> State -> (A.Audio, Effect))}
 
-applyFX :: Int -> A.Audio -> [Effect] -> (A.Audio, [Effect])
-applyFX quarterSamples =
+applyFX :: Double -> A.Audio -> [Effect] -> (A.Audio, [Effect])
+applyFX quartSamp =
   L.mapAccumL (\audio effect ->
-    run effect quarterSamples audio $ knobs effect)
-
+    run effect quartSamp audio $ state effect)
 
 
 
@@ -101,21 +105,24 @@ pickChunks :: Double -> Double -> [Chunk] -> [Chunk]
 pickChunks from to schedule =
   [x | x <- schedule, chunkStart x < to, chunkEnd x >= from]
 
+
 data Track = Bus
-  {busName :: String,
-   controls :: [(A.Routing, Track)],
-   busFX   :: [Effect]}
+  {busName   :: String,
+   controls  :: [(A.Routing, Track)],
+   busFX     :: [Effect],
+   busFrozen :: Maybe Source}
   | Ragged
-  {raggedName :: String,
-   schedule   :: [Chunk],
-   raggedFX   :: [Effect],
-   pool       :: Array Source}
+  {raggedName   :: String,
+   schedule     :: [Chunk],
+   raggedFX     :: [Effect],
+   pool         :: Array Source,
+   raggedFrozen :: Maybe Source}
 
 data Source = Source
-  {sourceName :: String,
-   speed    :: Double,
-   sourceFX   :: [Effect],
-   audio    :: A.Audio}
+  {sourceName   :: String,
+   speed        :: Double,
+   sourceFX     :: [Effect],
+   audio        :: A.Audio}
 
 
 class Mixable m where
@@ -126,56 +133,78 @@ instance Mixable Source
 instance Mixable Track
 
 
-fromSource :: Int -> (Int, Int) -> Source -> (A.Audio, Source)
-fromSource quarterSamp (at, until) (Source name speed fx audio) =
-  (out, Source name speed newFX audio)
+class Tickable t where
+  
 
-  where at' = round $ fromIntegral at * speed
-        until' = round $ fromIntegral until * speed
+fromSource :: Double -> (Double, Double) -> Source -> (A.Audio, Source)
+
+fromSource quartSamp (at, until) s@(Source _ speed fx audio) =
+  (out, s {sourceFX=newFX})
+
+  where at' = round $ at * speed
+        until' = round $ until * speed
         toSend = A.sliceAudio at' until' audio
-        (out, newFX) = applyFX quarterSamp toSend fx
+        (out, newFX) = applyFX quartSamp toSend fx
 
 
-fromTrack :: Int
-           -> ([Chunk] -> [Chunk])
-           -> (Chunk -> (Int, Int))
-           -> Track
-           -> (A.Audio, Track)
-fromTrack quartSamp inScope borders (Ragged name schedule fx pool) =
-  let continue chunk = A.route (routing chunk) `first`
-        fromSource quartSamp (borders chunk) (pool `aget` link chunk)
-      visible = inScope schedule
+
+fromTrack :: Double
+          -> ([Chunk] -> [Chunk])
+          -> (Double -> (Double, Double))
+          -> Track
+          -> (A.Audio, Track)
+
+fromTrack quartSamp _ borders t@(Ragged _ _ _ _ (Just f)) =
+  let (out, newFrozen) = fromSource quartSamp (borders 0) f
+  in (out, t {busFrozen=Just newFrozen})
+
+
+fromTrack quartSamp inScope borders t@(Ragged _ schedule fx pool _) =
+  let continue (Chunk start _ link routing) = A.route routing `first`
+        fromSource quartSamp (borders start) (aget pool link)
+
+      visible = inScope $ schedule
       (raw, newSources) = mix $ continue <$> visible
-      (out, newFX) = applyFX quartSamp raw fx
+      (out, newFX) = applyFX quartSamp raw $ fx
       newPool = pool `aset` zip (link <$> visible) newSources
-  in (out, Ragged name schedule newFX newPool)
 
-fromTrack quartSamp inScope borders (Bus name controls fx) =
+  in (out, t {raggedFX=newFX, pool=newPool})
+
+
+fromTrack quartSamp _ borders t@(Bus _ _ _ (Just f)) =
+  let (out, newFrozen) = fromSource quartSamp (borders 0) f
+  in (out, t {busFrozen=Just newFrozen})
+
+
+fromTrack quartSamp inScope borders t@(Bus _ controls fx _) =
   let continue (routing, track) = A.route routing `first`
         fromTrack quartSamp inScope borders track
+
       (raw, newTracks) = mix $ continue <$> controls
       nowControls = zip (fst <$> controls) newTracks
-      (out, newFX) = applyFX quartSamp raw fx 
-  in (out, Bus name nowControls newFX)
- 
+      (out, newFX) = applyFX quartSamp raw $ fx
+
+  in (out, t {controls=nowControls, busFX=newFX})
+
 
 data Project = Project
   {name    :: String,
    tempi   :: [Tempo],
    master  :: Track,
    slider  :: Double,
-   rate    :: Int,
-   buffLen :: Int}
+   rate    :: Double,
+   buffLen :: Double}
+
 
 fill :: Project -> (A.Audio, Project)
-fill p@(Project name tempi master slider rate buffLen) =
+fill p@(Project {..}) =
   (out, p {slider=newSlider, master=newMaster})
+
   where quartSamp = noteSamples rate slider 4 tempi
-        newSlider = slider
-                  + bufferBeats rate slider (fromIntegral buffLen) tempi
+        newSlider = tick rate slider buffLen tempi
         inScope = pickChunks slider newSlider
-        borders chunk =
-          let at = windowSamples rate slider (chunkStart chunk) tempi
+        borders start =
+          let at = window rate slider start tempi
               until = at + buffLen
           in (at, until)
         (out, newMaster) = fromTrack quartSamp inScope borders master
